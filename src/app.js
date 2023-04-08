3f84fe2e44a2b0274db0beb8a7051a8b8ef8cc6d
tanzeelkhan95 <turbogamer95@gmail.com>
2023-04-09 02:20:06 +0500

    setting up MongoDB and patials (for navbar)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require("express")
 const hbs = require("hbs")
 const app = express()
+const mongoose = require("mongoose")
 
 const routes = require('./routes/main')
 
+app.use('/static', express.static("public"))
 app.use('',routes)
 
 //(template engine)
 
 app.set('view engine', 'hbs')
 app.set("views","views")
+hbs.registerPartials("views/partials")
 
+//db connections
+mongoose.connect("mongodb://localhost/thesis_project",()=>{
+    console.log("db connected")
+})
 
 app.listen(process.env.PORT | 5556,()=>{
     console.log("server started")
