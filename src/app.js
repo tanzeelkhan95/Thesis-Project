6e079cda7905f008a4df4788049d6af046b8d8a9
Mohammad Tanzeel Ur Rehman <72350513+tanzeelkhan95@users.noreply.github.com>
2023-05-23 13:23:43 +0500

    Made the slider completely dynamic by making a slider schema and sending it to database by using create

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require("mongoose")
 const Product = require("./models/Product")
 const Detail = require("./models/Detail")
+const Slider = require("./models/Slider")
 
 const routes = require('./routes/main')
 
@@ -19,6 +20,34 @@ hbs.registerPartials("views/partials")
 //db connections
 mongoose.connect("mongodb://localhost/thesis_project",()=>{
     console.log("db connected")
+
+    /*------------------  CREATE SLIDER SCHEMA ----------------------- */
+
+    // Slider.create([
+    //     {
+    //         title:"GET THE LOOK",
+    //         subTitle:"SHOP OUR LATEST STYLES TODAY!",
+    //         imageUrl:"/static/images/DISPLAY 1.jpg",
+    //     },
+    //     {
+    //         title:"DON'T MISS OUT",
+    //         subTitle:"LIMITED TIME ONLY SALE ITEMS",
+    //         imageUrl:"/static/images/DISPLAY 2.jpg",
+    //     },
+    //     {
+    //         title:"SHOP WITH PURPOSE",
+    //         subTitle:"WITH OUR LATEST SUITS COLLECTION, STAND OUT FROM THE REST",
+    //         imageUrl:"/static/images/DISPLAY 3.jpg",
+    //     },
+    //     {
+    //         title:"GET THE LOOK",
+    //         subTitle:"GET 30% OFF ON YOUR FIRST PURCHASE BY USING CODE: FIRST",
+    //         imageUrl:"/static/images/DISPLAY 4.jpg",
+    //     },
+    // ])
+
+/*---------------------- CREATE DETAIL SCHEMA --------------------*/
+
     // Detail.create({
     //     brandIconUrl:"/static/images/logo.png",
     //     links:[
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,20 +3,37 @@ const express = require('express')
 const { route } = require('express/lib/application')
 
 const Detail = require("../models/Detail")
+const Slider = require('../models/Slider')
 
 const routes = express.Router()
 
 routes.get("/", async (req,res) => {
     const details = await Detail.findOne({"_id":"646bafa2c087c0d9d56028b9"})
+    const slider = await Slider.find()
     // console.log(details)
     res.render("index",{
-        details:details
+        details:details,
+        slides:slider,
     })
 })
 
 routes.get('/product', async(req,res)=>{
     const details = await Detail.findOne({"_id":"646bafa2c087c0d9d56028b9"})
     res.render("product", {
+        details:details
+    })
+})
+
+routes.get('/cart', async(req,res)=>{
+    const details = await Detail.findOne({"_id":"646bafa2c087c0d9d56028b9"})
+    res.render("cart",{
+        details:details
+    })
+})
+
+routes.get('/login', async(req,res)=>{
+    const details = await Detail.findOne({"_id":"646bafa2c087c0d9d56028b9"})
+    res.render("login",{
         details:details
     })
 })
